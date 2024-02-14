ca53ae216593c8120aa41f11650f6be14c3dddb1
Théo <theos9mar@gmail.com>
2024-02-14 16:29:33 +0100

    logo sur toute les pages

--- news.php
+++ news.php
@@ -11,7 +11,7 @@ session_start();
     </head>
     <body>
         <header>
-            <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
+            <a href='admin.php'><img src="logo.png" alt="Logo de notre réseau social"/></a>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
                 <a href="wall.php?user_id=<?php echo  $_SESSION['connected_id'] ?>">Mur</a>
